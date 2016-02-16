26e710329d18782b38045439293fcd6ecd9d92b8
claudio <letresorinterdit@gmail.com>
2016-02-16 18:59:06 -0300

    Prueba de timezone
    no funciono, sigue dando mejor resultado poniendo como variable de sistema -Duser.timezone

---
+++ src/calendario/timezone/Main.java
@@ -0,0 +1,34 @@
+package calendario.timezone;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class Main {
+
+	
+	
+	  public String s = "America/Santiago";
+      public Calendar hoy = new GregorianCalendar(TimeZone.getTimeZone(s));
+      public Date trialTime = new Date();
+      
+//      hoy.setTime(trialTime);
+	
+	public static void main(String[] args) {
+		
+		Main ob = new Main();
+		ob.hoy.setTime(ob.trialTime);
+		
+		System.out.println(ob.hoy);
+		System.out.println(ob.hoy.getTimeZone());
+		System.out.println(ob.hoy.getFirstDayOfWeek());
+		System.out.println(ob.hoy.getTime());
+		
+		
+
+	}
+	
+	
+
+}
