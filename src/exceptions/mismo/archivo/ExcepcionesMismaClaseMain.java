de4b22a40d3880b5d0a7d1a0712605a262359b7b
claudio <letresorinterdit@gmail.com>
2016-04-15 15:27:30 -0300

    Creando un servidor para acceder por telnet y luego modificar para tener como servicio de windows
    o daemon de linux

--- src/exceptions/mismo/archivo/ExcepcionesMismaClaseMain.java
+++ src/exceptions/mismo/archivo/ExcepcionesMismaClaseMain.java
@@ -11,17 +11,24 @@ public class ExcepcionesMismaClaseMain {
 		ExcepcionesMismaClaseMain ex = new ExcepcionesMismaClaseMain();
 	
 		ex.lanzaExcepcion();
+		System.out.println("ExcepcionesMismaClaseMain.main()");
 
 	}
 	
 	
 	public void lanzaExcepcion(){
-		
+		int a = 0;
+		String b="0b";
+		System.out.println(a);
 		try {
 			throw new IOException();
 		} catch (IOException e) {			
 			System.out.println("asdfadsf");
+//			e.printStackTrace();
 		}
+		
+		System.out.println(b);
+		
 	}
 	
 }
