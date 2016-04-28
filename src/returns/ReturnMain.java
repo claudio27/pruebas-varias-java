b091f1e985bd60fc0d1c3d10cc26cb81647ec0d6
claudio <letresorinterdit@gmail.com>
2016-04-28 16:08:22 -0300

    Probando usar return dentro de bloques if, para cortar la ejecucion de un metodo.

---
+++ src/returns/ReturnMain.java
@@ -0,0 +1,26 @@
+package returns;
+
+public class ReturnMain {
+
+	String a = "B";
+	
+	public static void main(String[] args){
+		
+		ReturnMain obj = new ReturnMain();
+		obj.metodo1();
+		
+	}
+	
+	public void metodo1(){
+		System.out.println("metodo1");
+		
+		if( "A".equals(this.a))
+			System.out.println("metodo2");
+		else
+			return; // Termina la funcion
+		
+		System.out.println("metodo3");
+		
+	}
+}
+
